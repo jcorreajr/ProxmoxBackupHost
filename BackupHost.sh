147f15610980686ccef13716e21422e6d396f6e9
jcorreajr <jcorreajr@gmail.com>
2020-12-16 15:14:55 -0300

    add senha para script rodar na Cron add envio de resultado por email

--- BackupHost.sh
+++ BackupHost.sh
@@ -7,10 +7,19 @@ usuariopbs="root@pam"
 ippbs="192.168.100.240"
 datastorepbs="DestinoBackup"
 
+# Colocar a senha do usuário de backup:
+export PBS_PASSWORD="123456"
+
+
 # Iniciando backup
 
 # Backup Conf
-proxmox-backup-client backup proxmoxEtc.pxar:/etc proxmoxRoot.pxar:/root proxmoxLib.pxar:/var/lib/pve-cluster --include-dev /etc/pve --verbose --repository $usuariopbs@$ippbs:$datastorepbs
+SAIDA=`proxmox-backup-client backup proxmoxEtc.pxar:/etc proxmoxRoot.pxar:/root proxmoxLib.pxar:/var/lib/pve-cluster --include-dev /etc/pve --verbose --repository $usuariopbs@$ippbs:$datastorepbs`
+
+# Enviando resultado por e-mail
+echo "$SAIDA" | mail -s "Backup host $ippbs" email@dominio.com.br
+# Resultado em tela
+echo "$SAIDA"
 
 # Backup de /var/lib/vz
 ## proxmox-backup-client backup proxmoxVz.pxar:/var/lib/vz --verbose --repository $usuariopbs@$ippbs:$datastorepbs
